a99209d450f15581a271f60915908d422c8955b9
Максим Буданов <maksimbudanov@MacBook-Pro-Maksim.local>
2023-01-25 03:05:56 +0300

    test

---
+++ tele_cat.py
@@ -0,0 +1,20 @@
+import time
+from telegram import Bot
+
+
+chat_id = 642411623 #замените на свое значение, подробнее ниже
+bot = Bot("5718308924:AAFDDxwBvSoOXZJHOZICW75RZPLD2pSO668")
+
+
+def send_random_cat():
+    url = f'https://cataas.com/cat?t=${time.time()}'
+    bot.send_photo(chat_id, url)
+
+
+def main() -> None:
+    send_random_cat()
+    print('Cat has been sent')
+
+
+if __name__ == "__main__":
+    main()
